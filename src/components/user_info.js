f0e4775ad05ce4be6805cf2942a1737372b36b4e
KaiHotz <kaihotz@me.com>
2017-03-14 21:39:26 +0100

    Removed some unnecessary text

--- src/components/user_info.js
+++ src/components/user_info.js
@@ -5,8 +5,8 @@ const UserInfo = (props) => {
   return (
     <div className="user-info">
       <img className="img-responsive center-block" src={props.info.owner.avatar_url} />
-      <h3>User Name: {props.info.owner.login}</h3>
-      <h4>GitHub: <a href={props.info.owner.html_url} target="_blank">{props.info.owner.html_url}</a></h4>
+      <h3>{props.info.owner.login}</h3>
+      <h4><a href={props.info.owner.html_url} target="_blank">{props.info.owner.html_url}</a></h4>
     </div>
   );
 };
